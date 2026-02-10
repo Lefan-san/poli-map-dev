3b57f17d8799d596cedd12a7c4b4669115fc5635
Lefan-san <ahmadattaimi@gmail.com>
2026-02-10 11:17:35 +0000

    update location

--- src/locations.js
+++ src/locations.js
@@ -4,7 +4,7 @@
 export const locations = [
     {
         name: "JKA (Civil Engineering)",
-        coords: [101.1218, 4.5891],
+        coords: [101.12575286445544,4.588208002181487],
         desc: "Main building for Civil Engineering students.",
         img: []
     },
@@ -28,27 +28,27 @@ export const locations = [
     },
     {
         name: "Library (Perpustakaan PUO)",
-        coords: [101.1225, 4.5880],
+        coords: [101.12434202259499,4.587566334653156],
         desc: "Learning resource center & e-library access.",
-        img: []
+        img: ["image/libbary/1.jpg"]
     },
     {
         name: "Dewan Jubli Perak",
-        coords: [4.587560151474034, 101.12381542138613],
+        coords: [ 101.12381542138613,4.587560151474034],
         desc: "Main auditorium for events and ceremonies.",
-        img: ["image/DEWAN_JUBLI_PERAK.jpg"]
+        img: ["image/dewan_jubli_perak/1.jpg"]
     },
     {
         name: "Cafe A (JKA Cafeteria)",
-        coords: [4.587886698466278, 101.12498833422116],
+        coords: [ 101.12498833422116,4.587886698466278],
         desc: "Cafeteria near Dewan Warisan.",
-        img: ["image/KANTIN_A.jpg"]
+        img: ["image/Cafe_A/1.jpg"]
     },
     {
         name: "Cafe B (JKE Cafeteria)",
         coords: [101.12275930567483, 4.586740727476954],
         desc: "Cafeteria near JKE building.",
-        img: ["image/cafe_jke/1.jpg", "image/cafe_jke/2.jpg"]
+        img: ["image/cafe_jke/1.jpg"]
     },
     {
         name: "Cafe C (Campus B Cafeteria)",
@@ -58,26 +58,56 @@ export const locations = [
     },
     {
         name: "Kolej Kediaman PFI",
-        coords: [4.589539460460139, 101.12152033870018],
+        coords: [101.121520338700184, 4.589539460460139],
         desc: "Student accommodation for PFI students.",
         img: []
     },   
     {
         name: "Dewan Seri Kinta",
-        coords: [4.588055803102894, 101.12531008087336],
+        coords: [101.12335727815913, 4.591170884772874],
         desc: "Main hall for events and activities.",
         img: ["image/DEWAN_KINTA.jpg"]
     },
         {
         name: "Dewan Warisan",
-        coords: [4.588055803102894, 101.12531008087336],
+        coords: [101.12523788034306,4.588106404855311],
         desc: "Main hall for events and activities.",
         img: ["image/dewan_warisan/1.jpg"]
     },
     {
         name: "Pondok Guard",
-        coords: [4.588740968020849, 101.12620219660123],
-        desc: "Safety Place.",
+        coords: [101.12612837373243,4.588721335782303],
+        desc: "Main hall for events and activities.",
         img: []
+    },
+    {
+       name:"Bangunan Jabatan Matematik Sains Komputer",
+        coords:[101.1247933709491,4.588312758939645],
+        desc:"Place for Student from Mathematic and Science",
+        img:["image/JMSK/1.jpg"]
+    },
+     {
+        name:"Pejabat Pentadbiran PUO",
+        coords:[101.12536828362298, 4.588349293664868],
+        desc:"Main administrative office for PUO.",
+        img:["image/pejabat_pentadbiran/1.jpg"]
+    },
+    {
+        name:"Surau PUO",
+        coords:[101.12173427144272, 4.5885820100253225],
+        desc:"Main prayer hall for PUO.",
+        img:["image/surau_puo/1.jpg"]
+    },
+    {
+        name:"Parking Lot PUO",
+        coords:[101.12398400055837, 4.588907145713752],
+        desc:"Main parking lot for PUO.",
+        img:["image/parking_lot/1.jpg"]
+    },
+    {
+        name:"Cafe Kamsis",
+        coords:[101.12140407985167, 4.590155199439324],
+        desc:"Cafe near Kamsis building.",
+        img:["image/cafe_kamsis/2.jpg"]
     }
 ];
